8d2035f493c61125396f35732e4d20c5a6ebd8d6
Jim Publicover <jameslpublicover@gmail.com>
2021-05-21 16:49:51 +0200

    bootstrap, pg, limited stylesheet

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.append('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    // i found the following bullshit on pubgrub, trying to swap a partial with ajax
+    // it is super important
+    jquery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 module.exports = environment
